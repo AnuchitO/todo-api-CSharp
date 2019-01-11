d424abc4fc21995dd677b12c8fe78f568af54313
AnuchitO <anuchitp@thoughtworks.com>
2019-01-11 14:38:43 +0700

    delete todo.

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -66,5 +66,20 @@ namespace TodoApi.Controllers
 
             return NoContent();
         }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<TodoItem>> DeleteTodoItem(long id)
+        {
+          var todoItem = await _context.TodoItems.FindAsync(id);
+          if (todoItem == null)
+          {
+            return NotFound();
+          }
+
+          _context.TodoItems.Remove(todoItem);
+          await _context.SaveChangesAsync();
+
+          return todoItem;
+        }
     }
 }
